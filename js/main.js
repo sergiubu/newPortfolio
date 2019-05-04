88d3b5d59834d12d5d31f8b8acaca9db4e0e1cbf
Sergiu <sil3ent@yahoo.com>
2019-05-04 20:19:49 +0300

    Add slideshow functionality

---
+++ js/main.js
@@ -0,0 +1,39 @@
+// Slideshow
+let slideIndex = 1;
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  const slides = document.getElementsByClassName('slides');
+  const dots = document.getElementsByClassName('dot');
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(' active', '');
+  }
+
+  slides[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].className += ' active';
+}
+
+// Make it go
+showSlides(slideIndex);
