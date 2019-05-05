edbd5283dfd094b06a6339ca65cafaaa9fc1ddc2
Sergiu <sil3ent@yahoo.com>
2019-05-05 15:09:23 +0300

    Implement smooth scroll

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
+// Smooth scroll
+$('.nav').on('click', function(e) {
+  if (this.hash !== '') {
+    e.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      800,
+      function() {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
+
 // Slideshow
 let slideIndex = 1;
 
